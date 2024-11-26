e57bfaac0ea559da9c5482a492c7c716dc83542e
tahaNada01 <nadataha2001@outlook.fr>
2024-11-26 11:20:16 +0100

    Footer

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from 'react-native';
+import { Text, View ,Alert } from 'react-native';
 import Login from '../../components/Login';
 import Signup from '@/components/Signup';
+import Footer from '../shared/Footer';
 
 export default function Home() {
   return (
-    <Signup/>
+    <Footer
+        onHomePress={() => Alert.alert('Accueil')}
+        onFavoritePress={() => Alert.alert('Favoris')}
+        onCartPress={() => Alert.alert('Camera')}
+        onProfilePress={() => Alert.alert('Profil')}
+      />
   );
 }
