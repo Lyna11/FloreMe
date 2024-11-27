aa6976f5f91e6d1a066fd34901f3b7ecc0d69dea
fatma bellout <lline1245@gmail.com>
2024-11-27 11:17:04 +0100

    update names

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from "expo-router";
 import Home from "./index";
 import ImagePickerComponent from "../plantid_api/imagePickerComponent";
 import Login from "../auth/login";
+import SearchPlant from "../pernual_api/searchPlant";
 export default function Layout() {
-  return <Login />;
+  return <SearchPlant />;
 }
